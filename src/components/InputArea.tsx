7a873cb6ea0e85450ca5bd993eedbaccf002d940
Priyanta Islam <islam.priyanta@gmail.com>
2024-06-30 17:32:20 +1000

    basic typing system implemented

--- src/components/InputArea.tsx
+++ src/components/InputArea.tsx
@@ -1,21 +1,33 @@
 import "./InputArea.css";
-import React, { useState, ChangeEvent } from "react";
+import React, { useState, ChangeEvent, useRef, useEffect } from "react";
+
 function InputArea() {
   const [inputValue, setInputValue] = useState<string>("");
+  const [text, setText] = useState(
+    "apple under bicycle they dream beside funny his kitchen it river on sunshine her book chair garden between music laptop she window their ocean apple under bicycle they dream beside beside funny his kitchen"
+  );
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setInputValue(e.target.value);
-    console.log(e.target.value);
+    const value = e.target.value;
+    setInputValue(value);
+
+    console.log(value);
   };
+
+  useEffect(() => {}, []);
+
+  // Split text based on input length
+  const splitIndex = inputValue.length;
+  const part1 = text.slice(0, splitIndex);
+  const part2 = text.slice(splitIndex);
+
   return (
     <div className="inputArea">
-      <p className="inputParagraph">
+      <div className="text-container">
+        <span style={{ color: "#bca441" }}>{part1}</span>
         <span className="blinking-cursor"></span>
-        The quick brown fox jumps over the lazy dog Swift zephyrs blow vexing
-        daft Jim the quick brown fox jumps over the lazy dog Swift zephyrs blow
-        vexing daft Jim the quick brown fox jumps over the lazy dog Swift
-        zephyrs blow vexing daft Jim
-      </p>
+        <span>{part2}</span>
+      </div>
       <input
         type="text"
         className="invisible-input"
